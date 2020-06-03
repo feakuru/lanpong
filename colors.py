ff2f6fe187ff861ca9043c0ce920d16436211454
Dmitry Orlov <feakuru@yandex-team.ru>
2020-06-03 21:28:04 +0300

    Color constants

---
+++ colors.py
@@ -0,0 +1,9 @@
+"""Color constants used in game."""
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GRAY = (125, 125, 125)
+LIGHT_BLUE = (64, 128, 255)
+GREEN = (0, 200, 64)
+YELLOW = (225, 225, 0)
+PINK = (230, 50, 230)
