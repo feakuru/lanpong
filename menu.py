258f6bc698016b758c78c3c6de9f92cb1d50dcbc
Dmitry Orlov <feakuru@yandex-team.ru>
2020-06-03 21:28:29 +0300

    Menu function template

---
+++ menu.py
@@ -0,0 +1,9 @@
+import pygame
+from .colors import WHITE
+
+MENU_SURFACE_SIZE = (200, 150)
+
+def get_menu_surface():
+    menu_surface = pygame.Surface(MENU_SURFACE_SIZE)
+    menu_surface.fill(WHITE)
+    menu_surface.set_alpha(128)  # 0 to 255
