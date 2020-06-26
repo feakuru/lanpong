17782df856de9254a4a26c3e3a83e7fd6e12c1ab
Dmitry Orlov <feakuru@yandex-team.ru>
2020-06-26 15:16:19 +0300

    Added fn for score surface

--- menu.py
+++ menu.py
@@ -3,17 +3,21 @@ import pygame
 from colors import WHITE, BLACK
 
 MENU_SURFACE_SIZE = (250, 50)
+SCORE_SURFACE_SIZE = (100, 50)
 MENU_ALPHA = 200  # 0 to 255
 
 def get_menu_surface():
     menu_surface = pygame.Surface(MENU_SURFACE_SIZE)
     menu_surface.fill(WHITE)
     menu_surface.set_alpha(MENU_ALPHA)
+    
     font = pygame.font.Font(os.path.abspath('./fonts/tron.ttf'), 10)
+    
     text_top = font.render("Q to quit", 1, BLACK)
     text_bottom = font.render("W, S / up, down to control", 1, BLACK)
     text_top_size = text_top.get_size()
     text_bottom_size = text_bottom.get_size()
+    
     menu_surface.blit(
         text_top,
         (
@@ -29,3 +33,24 @@ def get_menu_surface():
         )
     )
     return menu_surface
+
+
+def get_score_surface(score):
+    score_surface = pygame.Surface(SCORE_SURFACE_SIZE)
+    score_surface.fill(WHITE)
+    score_surface.set_alpha(MENU_ALPHA)
+    
+    font = pygame.font.Font(os.path.abspath('./fonts/tron.ttf'), 10)
+    
+    score_text = font.render(str(score), 1, BLACK)
+    score_text_size = score_text.get_size()
+    
+    menu_surface.blit(
+        score_text,
+        (
+            SCORE_SURFACE_SIZE[0] / 2 - score_text_size[0] / 2,
+            SCORE_SURFACE_SIZE[1] / 4 - score_text_size[1] / 2,
+        )
+    )
+
+    return score_surface
